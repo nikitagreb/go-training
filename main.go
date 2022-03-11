c6f18d46104158dfdf14fb0fd0165140d9b9dcc8
Никита Гребенников <nvgrebennikov@sbercloud.ru>
2022-03-11 16:54:15 +0300

    divide worked

--- main.go
+++ main.go
@@ -26,7 +26,17 @@ func About(w http.ResponseWriter, r *http.Request) {
 	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+
+}
+
 // addValues adds two integers and return the sum
 func addValues(x, y int) int {
 	return x + y
+}
+
+func divideValues(x, y float32) (float32, error) {
+	result := x / y
+
+	return result, nil
 }
