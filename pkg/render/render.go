ccf9faa5400d1da748d470cdb2143f0ccd033efc
nvgrebennikov <nikitagreb0204@mail.ru>
2022-03-19 09:24:31 +0300

    done

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -4,12 +4,58 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"path/filepath"
 )
 
+var functions = template.FuncMap{}
+
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
+
+	_, err := RenderTemplateTest(w)
+	if err != nil {
+		fmt.Println("error getting template cache:", err)
+	}
+
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
+	err = parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template:", err)
 	}
+}
+
+func RenderTemplateTest(w http.ResponseWriter) (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+	pages, err := filepath.Glob("./template/*.page.tmpl")
+	if err != nil {
+		return cache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		fmt.Println("This is page:", page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+
+		//ts.ParseGlob()
+		if err != nil {
+			return cache, err
+		}
+
+		matches, err := filepath.Glob("./template/*.layout.tmpl")
+		if err != nil {
+			return cache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./template/*.layout.tmpl")
+			if err != nil {
+				return cache, err
+			}
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, err
 }
